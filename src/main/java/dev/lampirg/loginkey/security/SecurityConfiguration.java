ba7e51fbf4e12170cbc2fa6d7d8b19c0341e284f
Lampirg <63813365+Lampirg@users.noreply.github.com>
2024-08-25 00:01:21 +0300

    add session fullness to security configuration
    Disable anonymous.
    Set securityContextRepository.
    Create sessions.
    Explicitly point to 401 and 403 statuses (for some reason Spring fails to do it properly).

--- src/main/java/dev/lampirg/loginkey/security/SecurityConfiguration.java
+++ src/main/java/dev/lampirg/loginkey/security/SecurityConfiguration.java
@@ -1,19 +1,24 @@
 package dev.lampirg.loginkey.security;
 
 import dev.lampirg.loginkey.security.filter.UserDtoAuthenticationFilter;
+import dev.lampirg.loginkey.security.session.ApiKeyContextRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.List;
@@ -30,6 +35,15 @@ public class SecurityConfiguration {
                         UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(registry -> registry
                         .anyRequest().authenticated())
+                .anonymous(AbstractHttpConfigurer::disable)
+                .securityContext(conf -> conf
+                        .securityContextRepository(new ApiKeyContextRepository()))
+                .sessionManagement(conf -> conf
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .exceptionHandling(conf -> conf
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                        .accessDeniedHandler((request, response, accessDeniedException) ->
+                                response.setStatus(HttpServletResponse.SC_FORBIDDEN)))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
