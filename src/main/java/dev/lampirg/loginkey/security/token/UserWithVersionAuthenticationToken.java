aa2207a1bb9d54e78dac26f4e4d361c320b93111
Lampirg <63813365+Lampirg@users.noreply.github.com>
2024-08-26 00:53:14 +0300

    do not erase client version from authentication

--- src/main/java/dev/lampirg/loginkey/security/token/UserWithVersionAuthenticationToken.java
+++ src/main/java/dev/lampirg/loginkey/security/token/UserWithVersionAuthenticationToken.java
@@ -26,12 +26,6 @@ public class UserWithVersionAuthenticationToken extends UsernamePasswordAuthenti
     }
 
     @Override
-    public void eraseCredentials() {
-        super.eraseCredentials();
-        version = null;
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
