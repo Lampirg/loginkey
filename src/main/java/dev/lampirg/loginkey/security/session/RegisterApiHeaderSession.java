d5b9e8538588644b1ea1768b981cef81c82d66de
Lampirg <63813365+Lampirg@users.noreply.github.com>
2024-08-27 00:46:22 +0300

    rework ApiKeyContextRepository and RegisterApiHeaderSession
    Make them more robust and SessionRegistry dependant.

--- src/main/java/dev/lampirg/loginkey/security/session/RegisterApiHeaderSession.java
+++ src/main/java/dev/lampirg/loginkey/security/session/RegisterApiHeaderSession.java
@@ -9,36 +9,21 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.random.RandomGenerator;
-
 @Component
 @RequiredArgsConstructor
 public class RegisterApiHeaderSession implements SessionAuthenticationStrategy {
 
     private final SessionRegistry sessionRegistry;
 
-    private final RandomGenerator randomGenerator = RandomGenerator.getDefault();
-
-    private final Set<String> alreadyGenerated = ConcurrentHashMap.newKeySet();
+    private final RandomSessionIdGenerator sessionIdGenerator;
 
     @Override
     public void onAuthentication(Authentication authentication, HttpServletRequest request, HttpServletResponse response) throws SessionAuthenticationException {
-        String key = generateKey();
-        sessionRegistry.registerNewSession(key, authentication.getPrincipal());
-        response.setHeader(ApiKeyContextRepository.AUTH_HEADER, key);
-    }
-
-    private String generateKey() {
         String key;
         do {
-            key = randomGenerator.ints('0', 'z' + 1)
-                    .filter(value -> Character.isAlphabetic(value) || Character.isDigit(value))
-                    .limit(15)
-                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                    .toString();
-        } while (alreadyGenerated.contains(key));
-        return key;
+            key = sessionIdGenerator.generateKey();
+        } while (sessionRegistry.getSessionInformation(key) != null);
+        sessionRegistry.registerNewSession(key, authentication.getPrincipal());
+        response.setHeader(ApiKeyContextRepository.AUTH_HEADER, key);
     }
 }
